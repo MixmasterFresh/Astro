a91f59a42b2cdab9c9a2e40d79090c3d866e3040
Austin S. Horning <austinseven@outlook.com>
2017-03-19 23:13:45 -0400

    Hopefully someone finds this whole astro thing useful

---
+++ compiler/Parser/parser.go
@@ -0,0 +1,8 @@
+package parser
+
+//go:generate cat *.peg.partial > astro.peg
+//go:generate peg astro.peg
+
+func Parse() {
+  
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+  "astro/compiler/parser"
+)
+
+func main(){
+  parser.Parse()
+}
